8b0b06ea0e3b5505f7cbdffb4d78042b2e36cbb2
yaojiejia <yaojiejia0715@gmail.com>
2025-04-18 20:08:02 -0400

    changed app.run

--- ec2_server/receive_load.py
+++ ec2_server/receive_load.py
@@ -47,4 +47,5 @@ def submit_data():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
