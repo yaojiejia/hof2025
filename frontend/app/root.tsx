86b62ab30097a9b31cf38b7549a11317e0effd5e
SewonKim0 <sewonkim0black@gmail.com>
2025-04-18 21:06:15 -0400

    Navbar: navbar component complete, navbar added as layout

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Navbar from "./routes/Components/Navbar";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,6 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Navbar />
         {children}
         <ScrollRestoration />
         <Scripts />
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,8 +1,9 @@
 import type { Route } from "./+types/home";
+import './home.css';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" }, //TODO change
+    { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
